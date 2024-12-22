e791692387d1db3e1ecda2c40368eca5e33b8250
Dmytro Khymytsia <dimahimitsya@gmail.com>
2024-12-22 14:40:02 +0100

    Added comments

--- main.c
+++ main.c
@@ -4,22 +4,22 @@ int main()
 {
     FILE *fw;
     char task[50], sign, quit, buffer[50];
-    if((fw = fopen("ToDoList.txt", "w+")) == NULL) return 1;
+    if((fw = fopen("ToDoList.txt", "w+")) == NULL) return 1;        //check readability
 
     printf("Hi, I'm ToDoList\n");
-    while(sign != 'Q')
+    while(sign != 'Q')      //working until user enter 'Q'
     {
         printf("Add your task: ");
-        fgets(task, sizeof(task), stdin);
-        fputs(task, fw);
+        fgets(task, sizeof(task), stdin);       //reading from standart input
+        fputs(task, fw);        //noting input into file
 
         printf("Tasks -> T\nContinue -> Enter\nLeave -> Q\n");
         scanf("%c", &sign);
-        getchar();
+        getchar();      //Clear input from '\n'
 
-        if(sign == 'T')
+        if(sign == 'T')     
         {
-            rewind(fw);
+            rewind(fw);     //back to the beginning of file
             printf("/////\n");
             while(fgets(buffer, sizeof(buffer), fw) != NULL)
             {
@@ -28,7 +28,7 @@ int main()
             printf("/////\n");
             printf("Continue -> Enter\nLeave -> Q\n");
             scanf("%c", &sign);
-            getchar();
+            getchar();      //Clear input from '\n'
         }
     }
     printf("Goodbye!");
